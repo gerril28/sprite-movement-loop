ac8866a3c8d6879f134eda7b16a28579f14eb862
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-10-03 09:55:44 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-
+input.onGesture(Gesture.LogoUp, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.pause(100)
+        Sprite.change(LedSpriteProperty.X, 1)
+        basic.pause(100)
+    }
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    for (let index = 0; index < 4; index++) {
+        Sprite.change(LedSpriteProperty.X, -1)
+        basic.pause(100)
+    }
+})
+let Sprite: game.LedSprite = null
+Sprite = game.createSprite(4, 0)
