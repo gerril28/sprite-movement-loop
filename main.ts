f06d94652ff12c9f93cd814b72186662245ae430
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-10-04 09:46:09 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,31 @@
-input.onGesture(Gesture.LogoUp, function () {
-    for (let index = 0; index < 4; index++) {
-        basic.pause(100)
-        Sprite.change(LedSpriteProperty.X, 1)
-        basic.pause(100)
-    }
-})
-input.onGesture(Gesture.TiltLeft, function () {
+input.onButtonPressed(Button.A, function () {
     for (let index = 0; index < 4; index++) {
         Sprite.change(LedSpriteProperty.X, -1)
         basic.pause(100)
     }
 })
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 4; index++) {
+        Sprite.change(LedSpriteProperty.X, 1)
+        Sprite.change(LedSpriteProperty.Y, 1)
+        basic.pause(100)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 4; index++) {
+        Sprite.change(LedSpriteProperty.Y, -1)
+        basic.pause(100)
+    }
+})
+input.onGesture(Gesture.TiltRight, function () {
+    for (let index = 0; index < 4; index++) {
+        Sprite.change(LedSpriteProperty.Y, 1)
+        Sprite.change(LedSpriteProperty.X, 1)
+        basic.pause(100)
+    }
+})
 let Sprite: game.LedSprite = null
 Sprite = game.createSprite(4, 0)
+basic.forever(function () {
+	
+})
