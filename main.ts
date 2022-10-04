174fcd064c12d7f85ca73f72f8242ade02fb7053
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-10-04 09:48:43 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,12 +6,18 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.AB, function () {
     for (let index = 0; index < 4; index++) {
-        Sprite.change(LedSpriteProperty.X, 1)
+        Sprite.change(LedSpriteProperty.X, -1)
         Sprite.change(LedSpriteProperty.Y, 1)
         basic.pause(100)
     }
 })
 input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 4; index++) {
+        Sprite.change(LedSpriteProperty.Y, -1)
+        basic.pause(100)
+    }
+})
+input.onGesture(Gesture.Shake, function () {
     for (let index = 0; index < 4; index++) {
         Sprite.change(LedSpriteProperty.Y, -1)
         basic.pause(100)
