d33bb48e9fcdc6ac5b3f40c8ec62334d3ecfd5d0
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-10-05 08:41:20 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,37 +1,31 @@
-input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index < 4; index++) {
-        Sprite.change(LedSpriteProperty.X, -1)
-        basic.pause(100)
-    }
-})
-input.onButtonPressed(Button.AB, function () {
-    for (let index = 0; index < 4; index++) {
-        Sprite.change(LedSpriteProperty.X, -1)
-        Sprite.change(LedSpriteProperty.Y, 1)
-        basic.pause(100)
-    }
-})
-input.onButtonPressed(Button.B, function () {
-    for (let index = 0; index < 4; index++) {
-        Sprite.change(LedSpriteProperty.Y, -1)
-        basic.pause(100)
-    }
-})
-input.onGesture(Gesture.Shake, function () {
-    for (let index = 0; index < 4; index++) {
-        Sprite.change(LedSpriteProperty.Y, -1)
-        basic.pause(100)
-    }
-})
-input.onGesture(Gesture.TiltRight, function () {
-    for (let index = 0; index < 4; index++) {
-        Sprite.change(LedSpriteProperty.Y, 1)
-        Sprite.change(LedSpriteProperty.X, 1)
-        basic.pause(100)
-    }
-})
-let Sprite: game.LedSprite = null
-Sprite = game.createSprite(4, 0)
+let Sprite = game.createSprite(4, 0)
 basic.forever(function () {
-	
+    for (let index = 0; index < 2; index++) {
+        for (let index = 0; index < 4; index++) {
+            Sprite.change(LedSpriteProperty.X, -1)
+            basic.pause(100)
+        }
+        for (let index = 0; index < 4; index++) {
+            Sprite.change(LedSpriteProperty.Y, 1)
+            Sprite.change(LedSpriteProperty.X, 1)
+            basic.pause(100)
+        }
+        for (let index = 0; index < 4; index++) {
+            Sprite.change(LedSpriteProperty.Y, -1)
+            basic.pause(100)
+        }
+        for (let index = 0; index < 4; index++) {
+            Sprite.change(LedSpriteProperty.X, -1)
+            Sprite.change(LedSpriteProperty.Y, 1)
+            basic.pause(100)
+        }
+        for (let index = 0; index < 4; index++) {
+            Sprite.change(LedSpriteProperty.Y, -1)
+            basic.pause(100)
+        }
+        for (let index = 0; index < 4; index++) {
+            Sprite.change(LedSpriteProperty.X, 1)
+            basic.pause(100)
+        }
+    }
 })
